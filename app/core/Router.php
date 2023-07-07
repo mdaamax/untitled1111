0f4b7fa80e5ec48681b7669a938c826c67c6120e
avilovmaksim5 <avilovmaksim5@mail.ru>
2023-07-07 20:47:50 +0400

    1

--- app/core/Router.php
+++ app/core/Router.php
@@ -7,7 +7,25 @@ class Router
     public function match ()
     {
         $url = trim($_SERVER['REQUEST_URI'],'/');
-        var_dump($url);
+//        var_dump($url);
+        if (!empty($url)){
+            $params = explode('/',$url);
+            if (!empty($params[0]) && !empty($params[1])){
+                $this -> params = [
+                    'controller' => $params[0],
+                    'action' => $params[1]
+                ];
+            }
+                else{
+                    return false;
+                }
+            } else{
+            $this -> params = [
+                'controller' => 'main',
+                'action' => 'index'
+            ];
+        }
+        return true;
     }
 
     public function run()
