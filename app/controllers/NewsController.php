38d395d1ed02a175eca8d45791159ede1e9fe812
avilovmaksim5 <avilovmaksim5@mail.ru>
2023-07-25 20:45:38 +0400

    1

---
+++ app/controllers/NewsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\initController;
+use app\lib\UserOperations;
+use app\models\NewsModels;
+
+class NewsController extends initController
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'rules' => [
+                    [
+                        'action' => ['list'],
+                        'roles' => [UserOperations::RoleUser, UserOperations::RoleAdmin],
+                        'matchCallback' => function () {
+                            $this->redirect('/user/login');
+                        }
+                    ],
+                    [
+                        'action' => ['add', 'edit'],
+                        'roles' => [UserOperations::RoleAdmin],
+                        'matchCallback' => function () {
+                            $this->redirect('/news/list');
+                        }
+                    ],
+                ]
+            ]
+        ];
+    }
+
+    public function actionList()
+    {
+        $this->view->title = 'Новости';
+        $news_model = new NewsModels();
+        $news = $news_model->getListNews();
+        $this->render('list', [
+            'sidebar' => UserOperations::getMenuLinks(),
+            'news' => $news,
+            'role' => UserOperations::RoleAdmin,
+        ]);
+    }
+
+    public function actionAdd()
+    {
+        $this->view->title = 'Добавление новости';
+        $error_message = '';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['btn_news_add_form'])) {
+            $news_data = !empty($_POST['news']) ? $_POST['news'] : null;
+            if (!empty($news_data)) {
+                $userModel = new NewsModels();
+                $result_add = $userModel->add($news_data);
+                if ($result_add['result']) {
+                    $this->redirect('/news/list');
+                } else {
+                    $error_message = $result_add['error_message'];
+                }
+            }
+        }
+        $this->render('add', [
+            'sidebar' => UserOperations::getMenuLinks(),
+            'error_message' => $error_message
+        ]);
+    }
+
+    public function actionEdit()
+    {
+        $this->view->title = 'Редактирование новости';
+        $news_id = !empty($_GET['news_id']) ? $_GET['news_id'] : null;
+        $news = null;
+        $error_message = '';
+        if (!empty($news_id)) {
+            $news_model = new NewsModels();
+            $news = $news_model->getNewsById($news_id);
+            if (empty($news)) {
+                $error_message = 'Новость не найдена';
+            }
+        } else {
+            $error_message = 'Отсутствует идентификатор записи!';
+        }
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['btn_news_edit_form'])) {
+            $news_data = !empty($_POST['news']) ? $_POST['news'] : null;
+            if (!empty($news_data)) {
+                $newsModel = new NewsModels();
+                $result_edit = $newsModel->edit($news_id, $news_data);
+                if ($result_edit['result']) {
+                    $this->redirect('/news/list');
+                } else {
+                    $error_message = $result_edit['error_message'];
+                }
+            }
+        }
+        $this->render('edit', [
+            'sidebar' => UserOperations::getMenuLinks(),
+            'news' => $news,
+            'error_message' => $error_message
+        ]);
+    }
+}
---
+++ app/models/NewsModels.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace app\models;
+
+use app\core\BaseModel;
+
+class NewsModels extends BaseModel
+{
+    public function add($news_data)
+    {
+        $result = false;
+        $error_message = '';
+        if (empty($news_data['title'])) {
+            $error_message .= "Введите наименование!<br>";
+        }
+        if (empty($news_data['short_description'])) {
+            $error_message .= "Введите краткое описание!<br>";
+        }
+        if (empty($news_data['description'])) {
+            $error_message .= "Введите наименование!<br>";
+        }
+        if (empty($error_message)) {
+            $result = $this->insert(
+                'INSERT INTO news(title,short_description, description , date_create , description)
+                        values (:title,:short_description, :description ,NOW(),:user_id)',
+                [
+                    'title' => $news_data['title'],
+                    'short_description' => $news_data['short_description'],
+                    'description' => $news_data['description'],
+                    'user_id' => $news_data['user']['id'],
+                ]
+            );
+        }
+    }
+
+    public function getListNews()
+    {
+        $result = null;
+        $news = $this->select('select * from news');
+        if (!empty($news)) {
+            $result = $news;
+        }
+        return $result;
+    }
+
+    public function edit($news_id,$news_data)
+    {
+        $result = false;
+        $error_message = '';
+
+        if (empty($news_id)) {
+            $error_message .= "Отсутствует инентификатор записи<br>";
+        }
+        if (empty($news_data['title'])) {
+            $error_message .= "Введите наименование!<br>";
+        }
+        if (empty($news_data['short_description'])) {
+            $error_message .= "Введите краткое описание!<br>";
+        }
+
+        if (empty($news_data['description'])) {
+            $error_message .= "Введите описание<br>";
+        }
+        if (empty($error_message)){
+            $result = $this -> update(
+                "Update news SET tittle = :title ,short_description = :short_description,
+                 description =: description where id = :id",
+                [
+                    'title' => $news_data['title'],
+                    'short_description' => $news_data['short_description'],
+                    'description' => $news_data['description'],
+                    'id' => $news_id,
+                ]
+            );
+        }
+        return [
+            'result' => $result,
+            'error_message' => $error_message
+        ];
+    }
+
+}
